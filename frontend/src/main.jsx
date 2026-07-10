7a88507ad246af204bff15f082dec68048acd316
Thomas Chapman <thoadchapman@gmail.com>
2026-07-10 13:21:21 -0300

    refactor(frontend): migrar AppContext para TypeScript e adicionar suporte a originalString

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AppProvider } from './AppContext.jsx'
+import { AppProvider } from './AppContext'
 import App from './App.jsx'
 import './index.css'
 
@@ -8,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
       <App />
-    </AppProvider>    
+    </AppProvider>
   </StrictMode>,
 )
