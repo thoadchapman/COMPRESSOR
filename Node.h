af0d8b11994bd838aca3ec0b9f63c38770eb9eaf
Thomas Chapman <thoadchapman@gmail.com>
2026-07-13 15:16:10 -0300

    refactor: tornar metodos de Node inline e adicionar operador maior que

--- Node.h
+++ Node.h
@@ -9,18 +9,45 @@ private:
   Node *dir;
 
 public:
-  Node(int freq, unsigned char c);
-  Node(int freq, Node *esq, Node *dir);
-  bool operator<(const Node &outro) const;
-  int getFreq() const;
-  unsigned char getChar() const;
-  Node *getEsq() const;
-  Node *getDir() const;
-  void setFreq(int freq);
-  void setChar(unsigned char c);
-  void setEsq(Node *esq);
-  void setDir(Node *dir);
-  bool ehFolha();
+  Node(unsigned char c, int freq) {
+    this->c = c;
+    this->freq = freq;
+    this->esq = nullptr;
+    this->dir = nullptr;
+  }
+
+  Node(int freq, Node *esq, Node *dir) {
+    this->freq = freq;
+    this->c = 0;
+    this->esq = esq;
+    this->dir = dir;
+  }
+
+  bool operator<(const Node &outro) const {
+    if (this->freq != outro.getFreq()) {
+      return this->freq < outro.getFreq();
+    }
+    return this->c < outro.getChar();
+  }
+
+  bool operator>(const Node &outro) const {
+    if (this->freq != outro.getFreq()) {
+      return this->freq > outro.getFreq();
+    }
+    return this->c > outro.getChar();
+  }
+
+  int getFreq() const { return freq; }
+  unsigned char getChar() const { return c; }
+  Node *getEsq() const { return esq; }
+  Node *getDir() const { return dir; }
+
+  void setFreq(int freq) { this->freq = freq; }
+  void setChar(unsigned char c) { this->c = c; }
+  void setEsq(Node *esq) { this->esq = esq; }
+  void setDir(Node *dir) { this->dir = dir; }
+
+  bool ehFolha() { return (this->esq == nullptr && this->dir == nullptr); }
 };
 
 #endif
